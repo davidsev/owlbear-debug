022b0fa4c217cffdddbf4762c324a76f2135093d
David Severwright <code@davidsev.co.uk>
2024-08-01 23:33:50 +0100

    show messages if save fails

--- src/UI/Components/Metadata/BaseMetadataEditor.ts
+++ src/UI/Components/Metadata/BaseMetadataEditor.ts
@@ -3,14 +3,22 @@ import { html, PropertyValueMap } from 'lit';
 import { BaseElement } from '../../BaseElement';
 import { JsonEditor } from '../JsonEditor';
 import { Metadata } from '@owlbear-rodeo/sdk';
+import { baseCSS } from '../../baseCSS';
+import style from './BaseMetadataEditor.css';
 
 export abstract class BaseMetadataEditor extends BaseElement {
+
+    static styles = baseCSS(style);
 
     // Keep track of the original metadata so we can detect if anything is removed.
     private originalMetadata: Metadata = {};
 
     @query('json-editor')
     accessor jsonEditor!: JsonEditor;
+    @query('dialog')
+    accessor errorDialog!: HTMLDialogElement;
+    @query('dialog main')
+    accessor errorMessage!: HTMLElement;
 
     abstract hookMetadataChanged (callback: () => void): void;
 
@@ -29,6 +37,14 @@ export abstract class BaseMetadataEditor extends BaseElement {
     render () {
         return html`
             <json-editor json="&quot;Loading...&quot;" @change="${this.editorChanged}"></json-editor>
+            <dialog @click=${this.hideDialog}>
+                <div class="row">
+                    <div id="close" @click="${this.hide}">&times;</div>
+                </div>
+                <main>
+                    Error!
+                </main>
+            </dialog>
         `;
     }
 
@@ -48,6 +64,27 @@ export abstract class BaseMetadataEditor extends BaseElement {
             }
         }
 
-        return this.setMetadata(newMetadata);
+        try {
+            await this.setMetadata(newMetadata);
+        } catch (e: any) {
+            console.error(e);
+            this.errorMessage.textContent = JSON.stringify(e, undefined, 2);
+            this.errorDialog.showModal();
+        }
+    }
+
+    public show () {
+        this.errorDialog.showModal();
+    }
+
+    public hide () {
+        this.errorDialog?.close();
+    }
+
+    private hideDialog (e: PointerEvent) {
+        // Clicks within the dialog should have target set to the div or one of its children.
+        // If the target is the dialog itself, then the click was on the backdrop.
+        if (e.target == this.errorDialog)
+            this.errorDialog.close();
     }
 }
